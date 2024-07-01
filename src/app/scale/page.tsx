7416ba83f7b28a8e98054acb819469b17b412be5
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-01 23:10:20 +0530

    feat: Implement scale page content

--- src/app/scale/page.tsx
+++ src/app/scale/page.tsx
@@ -1,5 +1,10 @@
+import ScaleImg from "public/performance.jpg"
+import Hero from "@/components/hero";
+
 export default function ScalePage() {
-    return <div>
-        Scale Page
-    </div>
+    return (
+        <div>
+            <Hero imgData={ScaleImg} imgAlt='steel factory' title='Scale your app to infinity'/>
+        </div>
+    );
 }
