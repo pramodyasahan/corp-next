f240273aaa952508df426a4d369e405d87131881
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-01 23:08:53 +0530

    feat: Implement reliability page content

--- src/app/reliability/page.tsx
+++ src/app/reliability/page.tsx
@@ -1,5 +1,10 @@
+import reliabilityImg from "public/reliability.jpg"
+import Hero from "@/components/hero";
+
 export default function ReliabilityPage() {
-    return <div>
-        Reliability Page
-    </div>
+    return (
+        <div>
+            <Hero imgData={reliabilityImg} imgAlt='car factory' title='Super high reliability hosting'/>
+        </div>
+    );
 }
